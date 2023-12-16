fdcb2a6c73dd40442111fca47ff039bfb581b3aa
byfarm <buckshootroger@gmail.com>
2023-12-16 10:29:16 -0800

    basic config almost finished

--- init.lua
+++ init.lua
@@ -1,59 +1,68 @@
-
+-- set leader key to space
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	}
+    vim.fn.system {
+	    'git',
+	    'clone',
+	    '--filter=blob:none',
+	    'https://github.com/folke/lazy.nvim.git',
+	    '--branch=stable',
+	    lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
 
+-- ===================== install plugins ===============================
 require('lazy').setup({
-	-- git
-	'tpope/vim-fugitive',
-	'tpope/vim-rhubarb',
-	'lewis6991/gitsigns.nvim',
+    -- git
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+    'lewis6991/gitsigns.nvim',
 
-	-- tabs
-	'tpope/vim-sleuth',
-	'tpope/vim-surround',
+    -- tabs
+    'tpope/vim-sleuth',
+    'tpope/vim-surround',
 
-	-- sorround brackets
-	'm4xshen/autoclose.nvim',
+    -- sorround brackets
+    'm4xshen/autoclose.nvim',
 
-	-- writing tool
-	'junegunn/goyo.vim',
+    -- writing tool
+    'junegunn/goyo.vim',
 
-	-- tmux
-	"christoomey/vim-tmux-navigator",
+    -- tmux
+    "christoomey/vim-tmux-navigator",
 
-	-- colorscheme
-        {
-	    --'neanias/everforest-nvim',
-	    --'navarasu/onedark.nvim',
-	    'rose-pine/neovim',
-	    priority = 1000,
-	    config = function()
-	      vim.cmd.colorscheme 'rose-pine'
-	    end,
-	},
+    -- colorscheme
+    {
+	--'neanias/everforest-nvim',
+	--'navarasu/onedark.nvim',
+	'rose-pine/neovim',
+	priority = 1000,
+	config = function()
+	  vim.cmd.colorscheme 'rose-pine'
+	end,
+    },
 
-	-- bottom status bar
-	'nvim-lualine/lualine.nvim',
+    -- bottom status bar
+    'nvim-lualine/lualine.nvim',
 
-	-- comment blocks
-	{ 'numToStr/Comment.nvim', opts = {} },
+    -- comment blocks
+    { 'numToStr/Comment.nvim', opts = {} },
+
+    --{ 'folke/which-key.nvim',  opts = {} }, -- show keybinds
+
+    require 'plugins.lsp', -- call lsp setup
+    require 'plugins.autocomplete', -- call autocompletion
+    require 'plugins.telescope', -- fuzy finder
+    require 'plugins.treesitter', -- tree sitter for highlighting
 })
 
 
+-- ===================== Setup Plugins ==============================
 require("autoclose").setup({
   keys = {
     ["("] = { escape = false, close = true, pair = "()" },
@@ -98,9 +107,31 @@ require("lualine").setup({
     }
 })
 
---vim.cmd('source ~/.config/nvim/lua/custom/plugins/goyo.vim')
+require("telescope").setup {
+    defaults = {
+	mappings = {
+	  i = {
+	    ['<C-u>'] = false,
+	    ['<C-d>'] = false,
+	  },
+	},
+    },
+}
+
+pcall(require('telescope').load_extension, 'fzf')
+
+vim.cmd('source ~/.config/nvim/configure/goyo.vim')
+require 'configure.telescope'
+require 'configure.treesitter'
+require 'configure.lsp'
+require 'configure.autocomplete'
 
 -- =========================== OPTIONS =====================
+-- tabs
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
 -- line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
@@ -116,3 +147,14 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 vim.o.termguicolors = true
+
+
+-- highlight on yank 
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+	vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
---
+++ plugins/lsp.lua
@@ -0,0 +1,10 @@
+return {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+	{ 'williamboman/mason.nvim', config = true }, -- lsp package manager
+	'williamboman/mason-lspconfig.nvim', -- lsp package manager
+	"hrsh7th/nvim-cmp", -- auto completion
+	{ 'j-hui/fidget.nvim', tag = 'legacy', opts = {} }, -- progress message
+	'folke/neodev.nvim', -- plugin signature help
+    }
+}
